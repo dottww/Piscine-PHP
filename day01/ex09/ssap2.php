45216af2072300ab77d1c12357272c2f6e7951d8
Wei-chen LIN <weilin@student.42.fr>
2019-12-11 16:21:45 +0100

    ex09

--- day01/ex09/ssap2.php
+++ day01/ex09/ssap2.php
@@ -3,10 +3,14 @@
 
 $i = 1;
 
+print_r($argv);
+
 while ($i < $argc) {
 	$tmp = $tmp . ' ' . $argv[$i];
 	$i++;
 }
+
+echo "tmp=\"$tmp\"\n";
 
 $tmp = array_filter(explode(" ", $tmp), 'strlen');
 natcasesort($tmp);
