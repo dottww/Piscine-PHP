0f654284faf364d19705e9678a4412627193c084
Wei-chen LIN <weilin@student.42.fr>
2019-12-18 13:00:19 +0100

    6.0

--- d6/ex00/Color.class.php
+++ d6/ex00/Color.class.php
@@ -1,5 +1,24 @@
 <?php
 
+Class Color{
+	Public $tstr= a;
+	static $verbose = FALSE;
 
+	Public function __construct(){
+		print('__construct called' . PHP_EOL);
+		return;
+	}
+	public function __destruct(){
+		print('__destruct called' . PHP_EOL);
+		return;
+	}
+	public function __to_string(){
+		print('__to_string called' . PHP_EOL);
+	}
+	public static function doc(){
+		echo file_get_contents("Color.doc.txt");
+	}
+	
+}
 
 ?>
