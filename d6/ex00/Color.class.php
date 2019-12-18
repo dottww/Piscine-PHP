98eaab787f2eb7d128531b3bf0bc7790c4ef8834
Wei-chen LIN <weilin@student.42.fr>
2019-12-18 14:15:24 +0100

    6.0.1

--- d6/ex00/Color.class.php
+++ d6/ex00/Color.class.php
@@ -1,24 +1,46 @@
 <?php
 
-Class Color{
-	Public $tstr= a;
-	static $verbose = FALSE;
+class Color
+{
+	public $red;
+	public $green;
+	public $blue;
+	public static $verbose = FALSE;
 
-	Public function __construct(){
-		print('__construct called' . PHP_EOL);
+	public function __construct(array $kwargs)
+	{
+		if (array_key_exists('arg', $kwargs)) {
+			$intrgb = intval($kwargs['rgb']);
+			$this->red = ($intrgb >> 16) & 0xff;
+			$this->green = ($intrgb >> 8) & 0xff;
+			$this->blue = ($intrgb >> 0) & 0xff;
+		}
+		elseif (
+			array_key_exists('red', $kwargs) &&
+			array_key_exists('green', $kwargs) &&
+			array_key_exists('blue', $kwargs)
+		) {
+			$this->red = intval($kwargs['red']);
+			$this->green = intval($kwargs['green']);
+			$this->blue = intval($kwargs['blue']);
+		}
+		if(self::$verbose==TRUE)
+		{
+			
+		}
 		return;
 	}
-	public function __destruct(){
+	public function __destruct()
+	{
 		print('__destruct called' . PHP_EOL);
 		return;
 	}
-	public function __to_string(){
+	public function __to_string()
+	{
 		print('__to_string called' . PHP_EOL);
 	}
-	public static function doc(){
+	public static function doc()
+	{
 		echo file_get_contents("Color.doc.txt");
 	}
-	
 }
-
-?>
